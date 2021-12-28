2e4491c918f5b4eecf73c16e203896e77d2e07a4
SsangTa <96719791+SsangTa@users.noreply.github.com>
2021-12-28 09:15:42 +0900

    Update Hellojava.java

--- hellojava/src/com/yedam/java/main/Hellojava.java
+++ hellojava/src/com/yedam/java/main/Hellojava.java
@@ -3,7 +3,7 @@ package com.yedam.java.main;
 public class Hellojava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Wellcome to Java");
 
 	}
 
